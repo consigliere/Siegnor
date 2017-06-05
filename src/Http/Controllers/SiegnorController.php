5d0264236ceb616ae9ea8b5512411b3dda2ae4e2
anonymoussc <50c5ac69@opayq.com>
2017-06-05 07:28:21 +0700

    Modified src/Http/Controllers/SiegnorController.php

--- src/Http/Controllers/SiegnorController.php
+++ src/Http/Controllers/SiegnorController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
-class AssignController extends Controller
+class SiegnorController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -28,7 +28,9 @@ class AssignController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
      * @param  Request $request
+     *
      * @return Response
      */
     public function store(Request $request)
@@ -46,7 +48,9 @@ class AssignController extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
      * @param  Request $request
+     *
      * @return Response
      */
     public function update(Request $request)
