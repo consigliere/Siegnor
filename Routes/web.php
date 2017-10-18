b0ab8ef0a03f53dbd90c7682bbc094b20f3f9a34
anonymoussc <50c5ac69@opayq.com>
2017-10-18 22:58:18 +0700

    Modified web Routes

--- Routes/web.php
+++ Routes/web.php
@@ -6,5 +6,4 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'siegnor', 'namespace' => 'App\Components\Siegnor\Http\Controllers'], function () {
     Route::get('/', 'SiegnorController@index');
-    Route::get('/oauth2', 'SiegnorController@oauth2')->middleware('admin.user');
 });
